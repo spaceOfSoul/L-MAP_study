acb7ff31ca7613b7fc8c0954b529a38634ee27e5
spaceOfSoul <ychh1123@gmail.com>
2023-06-27 17:39:20 +0900

    re commit

--- testbed.py
+++ testbed.py
@@ -1,194 +1,177 @@
-from torch.utils.data import DataLoader
-import pandas as pd
 import numpy as np
-from torch.utils.data import Dataset
+import pandas as pd
 import os
-import pickle
+import glob
 import torch
+
+from torch.utils.data import Dataset
+from torch.utils.data import DataLoader
 from torch import nn
 from torch import optim
 
-forder= os.listdir('1월 데이터')
 
-forder.sort()
-# print(forder)
+#===================================== Weather and Polar voltaic Dataset =================================#
+class WPD(Dataset):
+	def __init__(self, weather_list, energy_list, region_ID):
+		self.wlist = weather_list	# all files for weather info
+		self.elist = energy_list	# all files for power gener.
+		self.rID   = region_ID
+		
+	def __len__(self):
+		return len(self.wlist)
 
-# exit()
-weather_by_region = {}
-for file in forder:
-    csv = pd.read_csv('1월 데이터/' + file, encoding='CP949')
-    csv = csv.drop(['지점명'], axis=1)
-    groups = csv.groupby(csv.columns[0])
-    for i in groups:
-        if weather_by_region.get(i[0]) is not None:
-            weather_by_region[i[0]].append(list(i))
-        else:
-            weather_by_region[i[0]] = list(i)
+	def __getitem__(self, idx):
+		wfilepath = self.wlist[idx]
+		efilepath = self.elist[idx]
 
-type(weather_by_region[310][1])
+		############## weather data loading  #################
+		# Loading: weather data for all regions written by chy
+		csv = pd.read_csv(wfilepath, encoding='CP949')
+		csv = csv.drop(['지점명'], axis=1)
+		groups = csv.groupby(csv.columns[0])
+		weather_by_region = {}
+		for i in groups:
+			if weather_by_region.get(i[0]) is not None:
+				weather_by_region[i[0]].append(list(i))
+			else:
+				weather_by_region[i[0]] = list(i)
 
-for i in weather_by_region:
-    region_data=weather_by_region[i]
-    
-    concated_data = region_data[1]
-    size = len(region_data)
-    for j in range(2,size):
-        concated_data = pd.concat([concated_data, region_data[j][1]])
-    
-    region_data = concated_data.values
-    size=len(region_data)
-    # print(region_data[567])
-    result = []
-    sum = np.zeros(14).astype(float)
-    
-    for j in range(size):
-        row_data = region_data[j][2:].astype(float)
-        sum += row_data
-        if (j+1) % 60 == 0:
-            result.append(sum/60)
-            sum = np.zeros(14)
+		# Choose region & Time alignment
+		rid = self.rID
+		region_data = weather_by_region[rid]
+		region_data = region_data[1].values
+		weather_data= np.zeros([1440, 15])	# hard coding for 1 day, 14 features & time
+		timeflag    = np.ones(1440)
+		for i in range(len(region_data)):
+			timestamp = region_data[i][1]
+			date_, time_ = timestamp.split(' ')
+			data = region_data[i][2:].astype(float)
+			data = np.nan_to_num(data, nan=0)
 
-    weather_by_region[i]=result
-    
-powers = []
-forder = os.listdir('2022_01_energy/2022_01')
+			hh = int(time_[:2])
+			mm = int(time_[-2:])
+			idx = hh*60+mm - 1
 
-forder=sorted(forder,key=lambda x:int(x.split('.')[0]))
-# print(forder)
+			weather_data[idx,0] = idx
+			weather_data[idx,1:] = data
+			timeflag[idx] = 0
 
-for file in forder:
-    xlsx = pd.read_excel('2022_01_energy/2022_01/'+file, engine='openpyxl', skiprows=range(4))
-    xlsx = xlsx.iloc[:-1, :]  #row remove
-    powers.append(xlsx)
-# powers
+		# interpolation for missing data
+		idx = np.where(timeflag==1)[0]
+		indices, temp = [], []
+		if len(idx) == 1:
+			indices.append(idx)
+		else:
+			diff = np.diff(idx)
+			for i in range(len(diff)):
+				temp.append(idx[i].tolist())
+				if diff[i] == 1:
+					temp.append(idx[i+1])
+				else:
+					indices.append(np.unique(temp).tolist())
+					temp = []
+			if len(temp) > 0:	# add the last block
+				indices.append(temp)
+				temp = []
 
-# 모든 데이터프레임을 하나의 리스트로 합침.
-all_powers = [power for df in powers for power in df.to_numpy()]
+		for n in range(len(indices)):
+			idx = indices[n]
+			maxV, minV = np.max(idx).astype(int), np.min(idx).astype(int)
+			prev = weather_data[minV-1,:]
+			post = weather_data[maxV+1,:]
+			
+			nsteps = len(idx)
+			for i in range(nsteps):
+				weather_data[i+minV] = (nsteps-i)*prev/(nsteps+1) + (i+1)*post/(nsteps+1)
 
-combined_powers = pd.DataFrame(all_powers, columns=['Datetime', 'Power'])
-powers_np = combined_powers.to_numpy()
-powers_np
+		#np.save('data.npy', weather_data)
+		#np.save('idx.npy', timeflag)
+		weather_data = torch.tensor(weather_data)
 
-#삽입할 위치
-insert_positions = [0]+[i for i in range(23, len(powers_np), 24)]
+		############## energy data loading  #################
+		# Loading: power generation data written by chy
+		xlsx = pd.read_excel(efilepath, engine='openpyxl', skiprows=range(4))
+		xlsx = xlsx.iloc[:-1,:]	# row remove
+		power = xlsx.to_numpy()
+		power = pd.DataFrame(power, columns=['Datetime', 'Power'])
+		power_data = power.to_numpy()
+		power_data = power_data[:,1].astype(float)
+		power_data = torch.tensor(power_data)
 
-num_inserts = len(insert_positions)
+		return weather_data, power_data
 
-# 타임스탬프 하루씩 증가하게 생성
-timestamps = [pd.Timestamp('2022-01-01 00:00:00') + pd.Timedelta(days=i) for i in range(num_inserts)]
 
-# [타임스탬프 , 0.0]
-insert_values = np.array([[timestamps[i], 0.0] for i in range(num_inserts)], dtype=object)
+class PredModel(nn.Module):
+	def __init__(self, input_dim, hidden_dim, output_dim):
+		super(PredModel, self).__init__()
+		self.rnn = nn.RNN(input_size=input_dim, hidden_size=hidden_dim, batch_first=True, dtype=torch.double)
+		self.fc = nn.Linear(hidden_dim, output_dim)
 
-powers_np = np.insert(powers_np, insert_positions, insert_values, axis=0)
-
-seq_len = 5
-batch_size = 11
-
-class CustomDataset(Dataset):
-    def __init__(self, weather, energy, region, seq_len):
-        self.weather = weather
-        self.energy = energy
-        self.region = region
-        self.seq_len = seq_len
-    
-    def __len__(self):
-        return len(self.energy)-self.seq_len
-    
-    def __getitem__(self, idx):
-        energy = self.energy[idx+self.seq_len][1]
-        weather_data = []
+		self.fc.weight.data = self.fc.weight.data.double()
+		self.fc.bias.data = self.fc.bias.data.double()
         
-        for i in range(self.seq_len):
-            region_weather = self.weather[self.region][idx + i]
-            region_weather = np.nan_to_num(region_weather, nan=0)
-            weather_data.append(region_weather)
+	def forward(self, x):
+		out, hidden = self.rnn(x)
+		out = out[:, -1, :]
+		out = self.fc(out)
+		return out
 
-        # energy를 numpy 배열로 변환하고 차원을 추가
-        energy = np.array([energy])
-        weather_data = torch.tensor(weather_data)
-        
-        return weather_data, energy
 
-# 678 : 강릉 성산
-dataset = CustomDataset(weather_by_region, powers_np,678, seq_len)
-dataloader = DataLoader(dataset, batch_size=batch_size, shuffle=False, drop_last=True)
 
-for i, (x_data, y_data) in enumerate(dataloader):
-    print(f'{x_data.shape} {y_data.shape}')
+#=======================================================================================================#
 
-# exit()
+# weather_list = ['../../DB/weather/minute/AWS/1월/aws_gwd_20220101.csv.csv', '../../DB/weather/minute/AWS/1월/aws_gwd_20220102.csv.csv', '../../DB/weather/minute/AWS/1월/aws_gwd_20220103.csv.csv', '../../DB/weather/minute/AWS/1월/aws_gwd_20220104.csv.csv', '../../DB/weather/minute/AWS/1월/aws_gwd_20220105.csv.csv', '../../DB/weather/minute/AWS/1월/aws_gwd_20220106.csv.csv', '../../DB/weather/minute/AWS/1월/aws_gwd_20220107.csv.csv', '../../DB/weather/minute/AWS/1월/aws_gwd_20220108.csv.csv', '../../DB/weather/minute/AWS/1월/aws_gwd_20220109.csv.csv', '../../DB/weather/minute/AWS/1월/aws_gwd_20220110.csv.csv', '../../DB/weather/minute/AWS/1월/aws_gwd_20220111.csv.csv', '../../DB/weather/minute/AWS/1월/aws_gwd_20220112.csv.csv', '../../DB/weather/minute/AWS/1월/aws_gwd_20220113.csv.csv', '../../DB/weather/minute/AWS/1월/aws_gwd_20220114.csv.csv', '../../DB/weather/minute/AWS/1월/aws_gwd_20220115.csv.csv', '../../DB/weather/minute/AWS/1월/aws_gwd_20220116.csv.csv', '../../DB/weather/minute/AWS/1월/aws_gwd_20220117.csv.csv', '../../DB/weather/minute/AWS/1월/aws_gwd_20220118.csv.csv', '../../DB/weather/minute/AWS/1월/aws_gwd_20220119.csv.csv', '../../DB/weather/minute/AWS/1월/aws_gwd_20220120.csv.csv', '../../DB/weather/minute/AWS/1월/aws_gwd_20220121.csv.csv', '../../DB/weather/minute/AWS/1월/aws_gwd_20220122.csv.csv', '../../DB/weather/minute/AWS/1월/aws_gwd_20220123.csv.csv', '../../DB/weather/minute/AWS/1월/aws_gwd_20220124.csv.csv', '../../DB/weather/minute/AWS/1월/aws_gwd_20220125.csv.csv', '../../DB/weather/minute/AWS/1월/aws_gwd_20220126.csv.csv', '../../DB/weather/minute/AWS/1월/aws_gwd_20220127.csv.csv', '../../DB/weather/minute/AWS/1월/aws_gwd_20220128.csv.csv', '../../DB/weather/minute/AWS/1월/aws_gwd_20220129.csv.csv', '../../DB/weather/minute/AWS/1월/aws_gwd_20220130.csv.csv', '../../DB/weather/minute/AWS/1월/aws_gwd_20220131.csv.csv']
+weather_list = glob.glob('1월 데이터/*')
+print(weather_list)
+# energy_list = ['../../DB/NRE/polar_voltaic_GWNU_C9/2022_01/1.xlsx', '../../DB/NRE/polar_voltaic_GWNU_C9/2022_01/2.xlsx', '../../DB/NRE/polar_voltaic_GWNU_C9/2022_01/3.xlsx', '../../DB/NRE/polar_voltaic_GWNU_C9/2022_01/4.xlsx', '../../DB/NRE/polar_voltaic_GWNU_C9/2022_01/5.xlsx', '../../DB/NRE/polar_voltaic_GWNU_C9/2022_01/6.xlsx', '../../DB/NRE/polar_voltaic_GWNU_C9/2022_01/7.xlsx', '../../DB/NRE/polar_voltaic_GWNU_C9/2022_01/8.xlsx', '../../DB/NRE/polar_voltaic_GWNU_C9/2022_01/9.xlsx', '../../DB/NRE/polar_voltaic_GWNU_C9/2022_01/10.xlsx', '../../DB/NRE/polar_voltaic_GWNU_C9/2022_01/11.xlsx', '../../DB/NRE/polar_voltaic_GWNU_C9/2022_01/12.xlsx', '../../DB/NRE/polar_voltaic_GWNU_C9/2022_01/13.xlsx', '../../DB/NRE/polar_voltaic_GWNU_C9/2022_01/14.xlsx', '../../DB/NRE/polar_voltaic_GWNU_C9/2022_01/15.xlsx', '../../DB/NRE/polar_voltaic_GWNU_C9/2022_01/16.xlsx', '../../DB/NRE/polar_voltaic_GWNU_C9/2022_01/17.xlsx', '../../DB/NRE/polar_voltaic_GWNU_C9/2022_01/18.xlsx', '../../DB/NRE/polar_voltaic_GWNU_C9/2022_01/19.xlsx', '../../DB/NRE/polar_voltaic_GWNU_C9/2022_01/20.xlsx', '../../DB/NRE/polar_voltaic_GWNU_C9/2022_01/21.xlsx', '../../DB/NRE/polar_voltaic_GWNU_C9/2022_01/22.xlsx', '../../DB/NRE/polar_voltaic_GWNU_C9/2022_01/23.xlsx', '../../DB/NRE/polar_voltaic_GWNU_C9/2022_01/24.xlsx', '../../DB/NRE/polar_voltaic_GWNU_C9/2022_01/25.xlsx', '../../DB/NRE/polar_voltaic_GWNU_C9/2022_01/26.xlsx', '../../DB/NRE/polar_voltaic_GWNU_C9/2022_01/27.xlsx', '../../DB/NRE/polar_voltaic_GWNU_C9/2022_01/28.xlsx', '../../DB/NRE/polar_voltaic_GWNU_C9/2022_01/29.xlsx', '../../DB/NRE/polar_voltaic_GWNU_C9/2022_01/30.xlsx', '../../DB/NRE/polar_voltaic_GWNU_C9/2022_01/31.xlsx']
+energy_list = glob.glob('2022_01_energy/2022_01/*')
+#weather_list = ['../../DB/weather/minute/AWS/1월/aws_gwd_20220106.csv.csv']
+#energy_list = ['../../DB/NRE/polar_voltaic_GWNU_C9/2022_01/1.xlsx']
 
-class RNNModel(nn.Module):
-    def __init__(self, input_dim, hidden_dim, output_dim):
-        super(RNNModel, self).__init__()
-        self.rnn = nn.RNN(input_size=input_dim, hidden_size=hidden_dim, batch_first=True, dtype=torch.double)
-        self.fc = nn.Linear(hidden_dim, output_dim) # RNN 모델의 출력 값을 입력으로 받아들이고, 최종 출력 값을 예측하기 위한 선형 레이어를 정의
-        # 
-        # 가중치와 편향 텐서의 dtype을 double로 변경
-        self.fc.weight.data = self.fc.weight.data.double()
-        self.fc.bias.data = self.fc.bias.data.double()
-        
-    def forward(self, x):
-        out, hidden = self.rnn(x)
-        out = out[:, -1, :]  # 마지막 시퀀스의 결과만 사용
-        out = self.fc(out)  # 최종 결과
-        return out
+dset = WPD(weather_list, energy_list, 678)
+dloader = DataLoader(dset, batch_size=1, shuffle=False, drop_last=True)
 
-# 모델 생성
-input_dim = 14 # feature 14 + energy 1
+input_dim = 15 # feature 14 with time
 hidden_dim = 64
 output_dim = 1
-model = RNNModel(input_dim, hidden_dim, output_dim)
 
-# 손실 함수 정의
+model = PredModel(input_dim, hidden_dim, output_dim)
 criterion = nn.MSELoss()
-# 평균 제곱 오차 :
-# 예측값과 실제 값의 차이를 제곱해서 데이터의 개수로 나눈 거
-
-# 최적화 기법 선택
 optimizer = optim.Adam(model.parameters(), lr=0.01)
 
-epochs = 1000
+max_epoch = 1000
+seqLeng = 30
+nBatch  = 23
+prev_data = torch.zeros([seqLeng, input_dim])	# 14 is for featDim
 losses = []
+for epoch in range(max_epoch):
+	loss = 0
+	for i, (x, y) in enumerate(dloader):
+		x = x.squeeze()
+		x = torch.cat((prev_data, x), axis=0)
+		prev_data = x[-seqLeng:,:]
+		y = y.squeeze()
 
-for epoch in range(epochs):
-    for i, (x_data, y_data) in enumerate(dataloader):
-        output = model(x_data.double())
-        
-        # loss 계산
-        loss = criterion(output, y_data)  # output과 y_data의 차이를 손실로 계산
-        # print(f'output:{output}, y_data:{y_data}')
-        # 가중치 업데이트
-        optimizer.zero_grad()
-        loss.backward()
-        optimizer.step()
+		nLeng, nFeat = x.shape
+		batch_data = []
+		for j in range(nBatch):
+			stridx = j*60
+			endidx = j*60 + seqLeng
+			batch_data.append(x[stridx:endidx,:].view(1,seqLeng, nFeat))
+		batch_data = torch.cat(batch_data, dim=0)
 
-    losses.append(loss.item())
-    print(f'Epoch [{epoch+1}/{epochs}], Loss: {loss.item():.4f}')
+		output = model(batch_data)
+		loss += criterion(output.squeeze(), y)
 
-# 모델 평가 모드
-model.eval()
+	optimizer.zero_grad()
+	loss.backward()
+	optimizer.step()
 
-test_loss = 0
+	losses.append(loss.item())
+	print(f'Epoch [{epoch+1}/{max_epoch}], Loss: {loss.item():.4f}')
 
-for i, (x_data, y_data) in enumerate(dataloader):
-    
-    
-    output = model(x_data.double())  # (15, 1)
-    loss = criterion(output, y_data)
 
-    test_loss += loss.item()
 
-# 전체 데이터셋의 평균 손실값
-test_loss /= len(dataloader)
 
-print(f'Test Loss: {test_loss:.4f}')
+	
 
-import matplotlib.pyplot as plt
-
-plt.plot(range(epochs), losses)
-plt.xlabel('Epochs')
-plt.ylabel('Loss')
-plt.title('Training Loss')
-plt.show()
