e231b99839d5f96017d6c4b76dd8348fb4cf7df3
spaceOfSoul <ychh1123@gmail.com>
2023-06-28 06:17:58 +0900

    asos 하다 맘, minMax정규화

--- testbed_year.py
+++ testbed_year.py
@@ -8,7 +8,7 @@ from torch.utils.data import Dataset
 from torch.utils.data import DataLoader
 from torch import nn
 from torch import optim
-from sklearn.preprocessing import MinMaxScaler
+
 
 #===================================== Weather and Polar voltaic Dataset =================================#
 class WPD(Dataset):
@@ -17,7 +17,7 @@ class WPD(Dataset):
 		self.elist = energy_list	# all files for power gener.
 		self.rID   = region_ID
 		
-	def __len__(self):
+	def __len__(self): 
 		return len(self.wlist)
 
 	def __getitem__(self, idx):
@@ -40,8 +40,8 @@ class WPD(Dataset):
 		rid = self.rID
 		region_data = weather_by_region[rid]
 		region_data = region_data[1].values
-		weather_data= np.zeros([1440, 15])	# hard coding for 1 day, 14 features & time
-		timeflag    = np.ones(1440)
+		weather_data= np.zeros([1440, 16])	# hard coding for 1 day, 15 features & time
+		timeflag = np.ones(1440)
 		for i in range(len(region_data)):
 			timestamp = region_data[i][1]
 			date_, time_ = timestamp.split(' ')
@@ -87,8 +87,6 @@ class WPD(Dataset):
 		#np.save('data.npy', weather_data)
 		#np.save('idx.npy', timeflag)
 		weather_data = torch.tensor(weather_data)
-  		#minMax 정규화
-		weather_data = torch.tensor(self.scaler.fit_transform(weather_data))
 
 		############## energy data loading  #################
 		# Loading: power generation data written by chy
@@ -99,7 +97,7 @@ class WPD(Dataset):
 		power_data = power.to_numpy()
 		power_data = power_data[:,1].astype(float)
 		power_data = torch.tensor(power_data)
-        
+
 		return weather_data, power_data
 
 
@@ -121,17 +119,25 @@ class PredModel(nn.Module):
 #=======================================================================================================#
 
 # weather_list = ['../../DB/weather/minute/AWS/1월/aws_gwd_20220101.csv.csv', '../../DB/weather/minute/AWS/1월/aws_gwd_20220102.csv.csv', '../../DB/weather/minute/AWS/1월/aws_gwd_20220103.csv.csv', '../../DB/weather/minute/AWS/1월/aws_gwd_20220104.csv.csv', '../../DB/weather/minute/AWS/1월/aws_gwd_20220105.csv.csv', '../../DB/weather/minute/AWS/1월/aws_gwd_20220106.csv.csv', '../../DB/weather/minute/AWS/1월/aws_gwd_20220107.csv.csv', '../../DB/weather/minute/AWS/1월/aws_gwd_20220108.csv.csv', '../../DB/weather/minute/AWS/1월/aws_gwd_20220109.csv.csv', '../../DB/weather/minute/AWS/1월/aws_gwd_20220110.csv.csv', '../../DB/weather/minute/AWS/1월/aws_gwd_20220111.csv.csv', '../../DB/weather/minute/AWS/1월/aws_gwd_20220112.csv.csv', '../../DB/weather/minute/AWS/1월/aws_gwd_20220113.csv.csv', '../../DB/weather/minute/AWS/1월/aws_gwd_20220114.csv.csv', '../../DB/weather/minute/AWS/1월/aws_gwd_20220115.csv.csv', '../../DB/weather/minute/AWS/1월/aws_gwd_20220116.csv.csv', '../../DB/weather/minute/AWS/1월/aws_gwd_20220117.csv.csv', '../../DB/weather/minute/AWS/1월/aws_gwd_20220118.csv.csv', '../../DB/weather/minute/AWS/1월/aws_gwd_20220119.csv.csv', '../../DB/weather/minute/AWS/1월/aws_gwd_20220120.csv.csv', '../../DB/weather/minute/AWS/1월/aws_gwd_20220121.csv.csv', '../../DB/weather/minute/AWS/1월/aws_gwd_20220122.csv.csv', '../../DB/weather/minute/AWS/1월/aws_gwd_20220123.csv.csv', '../../DB/weather/minute/AWS/1월/aws_gwd_20220124.csv.csv', '../../DB/weather/minute/AWS/1월/aws_gwd_20220125.csv.csv', '../../DB/weather/minute/AWS/1월/aws_gwd_20220126.csv.csv', '../../DB/weather/minute/AWS/1월/aws_gwd_20220127.csv.csv', '../../DB/weather/minute/AWS/1월/aws_gwd_20220128.csv.csv', '../../DB/weather/minute/AWS/1월/aws_gwd_20220129.csv.csv', '../../DB/weather/minute/AWS/1월/aws_gwd_20220130.csv.csv', '../../DB/weather/minute/AWS/1월/aws_gwd_20220131.csv.csv']
-weather_list = glob.glob('1월 데이터/*')
-print(weather_list)
+# weather_list = glob.glob('1월 데이터/*')
+weather_list=[]
+folders = glob.glob('ASOS_gwd_2022_data/*')
+for folder in folders:
+    weather_list.extend(glob.glob(f'{folder}/*'))
+
 # energy_list = ['../../DB/NRE/polar_voltaic_GWNU_C9/2022_01/1.xlsx', '../../DB/NRE/polar_voltaic_GWNU_C9/2022_01/2.xlsx', '../../DB/NRE/polar_voltaic_GWNU_C9/2022_01/3.xlsx', '../../DB/NRE/polar_voltaic_GWNU_C9/2022_01/4.xlsx', '../../DB/NRE/polar_voltaic_GWNU_C9/2022_01/5.xlsx', '../../DB/NRE/polar_voltaic_GWNU_C9/2022_01/6.xlsx', '../../DB/NRE/polar_voltaic_GWNU_C9/2022_01/7.xlsx', '../../DB/NRE/polar_voltaic_GWNU_C9/2022_01/8.xlsx', '../../DB/NRE/polar_voltaic_GWNU_C9/2022_01/9.xlsx', '../../DB/NRE/polar_voltaic_GWNU_C9/2022_01/10.xlsx', '../../DB/NRE/polar_voltaic_GWNU_C9/2022_01/11.xlsx', '../../DB/NRE/polar_voltaic_GWNU_C9/2022_01/12.xlsx', '../../DB/NRE/polar_voltaic_GWNU_C9/2022_01/13.xlsx', '../../DB/NRE/polar_voltaic_GWNU_C9/2022_01/14.xlsx', '../../DB/NRE/polar_voltaic_GWNU_C9/2022_01/15.xlsx', '../../DB/NRE/polar_voltaic_GWNU_C9/2022_01/16.xlsx', '../../DB/NRE/polar_voltaic_GWNU_C9/2022_01/17.xlsx', '../../DB/NRE/polar_voltaic_GWNU_C9/2022_01/18.xlsx', '../../DB/NRE/polar_voltaic_GWNU_C9/2022_01/19.xlsx', '../../DB/NRE/polar_voltaic_GWNU_C9/2022_01/20.xlsx', '../../DB/NRE/polar_voltaic_GWNU_C9/2022_01/21.xlsx', '../../DB/NRE/polar_voltaic_GWNU_C9/2022_01/22.xlsx', '../../DB/NRE/polar_voltaic_GWNU_C9/2022_01/23.xlsx', '../../DB/NRE/polar_voltaic_GWNU_C9/2022_01/24.xlsx', '../../DB/NRE/polar_voltaic_GWNU_C9/2022_01/25.xlsx', '../../DB/NRE/polar_voltaic_GWNU_C9/2022_01/26.xlsx', '../../DB/NRE/polar_voltaic_GWNU_C9/2022_01/27.xlsx', '../../DB/NRE/polar_voltaic_GWNU_C9/2022_01/28.xlsx', '../../DB/NRE/polar_voltaic_GWNU_C9/2022_01/29.xlsx', '../../DB/NRE/polar_voltaic_GWNU_C9/2022_01/30.xlsx', '../../DB/NRE/polar_voltaic_GWNU_C9/2022_01/31.xlsx']
-energy_list = glob.glob('2022_01_energy/2022_01/*')
+energy_list = []
+folders = glob.glob('GWNU_C9_발전량/*')
+for folder in folders:
+    energy_list.extend(glob.glob(f'{folder}/*'))
 #weather_list = ['../../DB/weather/minute/AWS/1월/aws_gwd_20220106.csv.csv']
 #energy_list = ['../../DB/NRE/polar_voltaic_GWNU_C9/2022_01/1.xlsx']
 
-dset = WPD(weather_list, energy_list, 678)
+# 105 : 강릉
+dset = WPD(weather_list, energy_list, 105)
 dloader = DataLoader(dset, batch_size=1, shuffle=False, drop_last=True)
 
-input_dim = 15 # feature 14 with time
+input_dim = 16 # feature 14 with time
 hidden_dim = 64
 output_dim = 1
 
@@ -169,9 +175,3 @@ for epoch in range(max_epoch):
 
 	losses.append(loss.item())
 	print(f'Epoch [{epoch+1}/{max_epoch}], Loss: {loss.item():.4f}')
-
-
-
-
-	
-
